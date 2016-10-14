cd990aa98b5458c103fe4f60eff046b276a1f4e5
Trev <trevor.pawlewicz@gmail.com>
2016-10-14 08:54:36 -0400

    added test mongo model

--- server/server.js
+++ server/server.js
@@ -2,9 +2,42 @@ var mongoose = require('mongoose');
 
 
 
-
-
-
-
-
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+
+
+
+// mongoose Model: -------------------
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+//------------------------------------
+
+
+
+
+
+
+
+// create a new instance of Todo
+var newTodo = new Todo({
+    text: 'Buy Flowers.',
+    completed: false,
+    completedAt: 8
+});
+
+// save() adds to mondo database. then() is a promise.
+newTodo.save().then((doc) => {
+    console.log('Saved:', doc);
+}, (err) => {
+    console.log('Unable to save.', err);
+});
